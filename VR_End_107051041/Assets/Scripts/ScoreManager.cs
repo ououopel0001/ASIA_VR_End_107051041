ec428f881c3b05111cb7aa4f00e639224985ccb6
ououopel0001 <71679877+ououopel0001@users.noreply.github.com>
2021-01-10 00:27:56 +0800

    球洞

--- VR_End_107051041/Assets/Scripts/ScoreManager.cs
+++ VR_End_107051041/Assets/Scripts/ScoreManager.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI; //引用文字介面
 
 public class ScoreManager : MonoBehaviour
 {
-    
+    [Header("分數介面")]
+    public Text TV;
+    [Header("分數")]
+    public int score;
 }
