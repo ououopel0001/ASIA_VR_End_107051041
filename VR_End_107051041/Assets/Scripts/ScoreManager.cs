1e942c7f068ec38f08e209604014ca47856e872b
ououopel0001 <71679877+ououopel0001@users.noreply.github.com>
2021-01-10 17:43:02 +0800

    音效

--- VR_End_107051041/Assets/Scripts/ScoreManager.cs
+++ VR_End_107051041/Assets/Scripts/ScoreManager.cs
@@ -7,4 +7,40 @@ public class ScoreManager : MonoBehaviour
     public Text TV;
     [Header("分數")]
     public int score;
+    //[Header("進球音效")]
+    //public AudioClip Sound;
+
+
+    private void OnTriggerEnter(Collider other) //進入感應區
+    {
+        if (other.tag =="撞球")
+        {
+            AddScore();
+        }
+
+        if (other.tag == "特殊撞球")
+        {
+            AddMoreScore();
+        }
+
+        if (other.transform.root.name == "Player")
+        {
+            print("歡迎來到公主撞球場");
+        }
+    }
+
+    //加分系統
+    private void AddScore()
+    {
+        score += 1;
+        TV.text = "Score : " + score;
+        
+    }
+
+    private void AddMoreScore()
+    {
+        score += 2;
+        TV.text = "Score : " + score;
+
+    }
 }
---
+++ VR_End_107051041/Assets/Scripts/ball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    AudioSource audio;
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+
+    {
+        if (collision.gameObject.name == "球桿")
+           {
+            audio.Play();
+            print("會進球嘛!!!");
+           }
+
+    }
+}
